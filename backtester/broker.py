9cfc9e77ec61b0c5e33c7fc59abb839bf7998f50
mmiah <mmiah@bu.edu>
2025-10-17 17:35:26 -0500

    writing broker code, need to update the position variable tho

--- backtester/broker.py
+++ backtester/broker.py
@@ -4,4 +4,16 @@ class Broker:
         self.position = 0
 
     def market_order(self, side: str, qty: int, price: float):
-        pass
+        # Broker: accepts market orders, updates cash/position with no slippage/fees (keep deterministic for tests).
+        
+        delta = 1 # amt to add to our cash balance 
+
+        if side == "BUY": 
+            delta *= -1 
+        
+        delta = delta * qty * price 
+
+        if cash + delta >= 0: 
+            cash += delta 
+        else: 
+            raise Exception (f"Could not complete order {side} of {qty} shares @ ${price}. Not enough balance.")
